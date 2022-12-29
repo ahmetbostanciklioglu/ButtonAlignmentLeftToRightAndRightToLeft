e9bd9ee3646ec636d3d7f8edf76a1c7f198b0a68
AhmetBostanciklioglu <ahmet.bostancikli@arafas.com>
2022-12-30 00:06:29 +0300

    Commit update

--- UIButton/ViewController.swift
+++ UIButton/ViewController.swift
@@ -8,42 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController{
-
+    //Set button
     let button = UIButton()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //Configure button
         button.frame = CGRect(x: 136, y:341 , width: 120, height: 52)
         button.backgroundColor = .systemIndigo
         button.setTitle("LABEL".localized(), for: UIControl.State.normal)
         button.setTitleColor(.white, for: UIControl.State.normal)
         button.setImage(UIImage(systemName: "star.fill"), for: UIControl.State.normal)
         button.tintColor = .white
-//        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 6, bottom: 0, right: 0)
-
         view.addSubview(button)
+       
+        //button image positioning according to language 
         let preferredLanguage = NSLocale.preferredLanguages[0]
-        
         if preferredLanguage ==  "en" {
             button.semanticContentAttribute = .forceLeftToRight
         }else if preferredLanguage == "ar" {
             button.semanticContentAttribute = .forceRightToLeft
         }
     }
-
-    
-   
-    
- 
 }
 
-
-
-
-
-
-
+//Localization
 extension String {
     func localized(with comment: String = "") -> String {
         return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: comment)
